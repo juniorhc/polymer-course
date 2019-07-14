37ac755dcc7076af45a35b75d7fb3129ca92ff64
Júnior <junior.jaava@gmail.com>
2019-07-14 18:32:34 +0200

    Commiting: addEventListener, dispatchEvent and lodash library (function clone)  regarding to removeItem

--- polymer-todoApp/src/components/app.js
+++ polymer-todoApp/src/components/app.js
@@ -13,17 +13,16 @@ class TodoApp extends LitElement {
         let list = JSON.parse(localStorage.getItem('todo-list'));
         this.todoList = list === null ? [] : list;
 
-
         this.addEventListener('addItem', (e) => {
             this.todoList = e.detail.todoList;
         });
-
-    }
-
-    _firstRendered() {
-        this.addEventListener('addItem', (e) => {
-            this.todoList = e.detail.todoList;
+        this.addEventListener('removeItem', (e) => {
+            let index = this.todoList.map(function(item){return item.id}).indexOf(e.detail.itemId);
+            this.todoList.splice(index, 1);
+            this.todoList = _.clone(this.todoList);
+            localStorage.setItem('todo-list', JSON.stringify(this.todoList));
         });
+
     }
 
     render() {

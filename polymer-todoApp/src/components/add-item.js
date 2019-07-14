255e38bf53626b37433200ddbb46847012c68ac6
Júnior <junior.jaava@gmail.com>
2019-07-14 16:36:31 +0200

    Commiting list of items. Using components and pass values from one component to another

--- polymer-todoApp/src/components/add-item.js
+++ polymer-todoApp/src/components/add-item.js
@@ -1,6 +1,6 @@
-import {LitElement, html} from '@polymer/lit-element'
+import { LitElement, html } from '@polymer/lit-element'
 
-class AddItem extends LitElement{
+class AddItem extends LitElement {
     static get properties() {
         return {
             todoList: Array,
@@ -8,14 +8,14 @@ class AddItem extends LitElement{
         }
     }
 
-    constructor(){
+    constructor() {
         super();
         this.todoItem = ''; //F5 we going to lose this data.
         //we need to use localstorage to store data
     }
 
-    inputKeypress(e){
-        if(e.keyCode == 13){
+    inputKeypress(e) {
+        if (e.keyCode == 13) {
             //call add item function
         } else {
             this.todoItem = e.target.value;
@@ -23,33 +23,35 @@ class AddItem extends LitElement{
         console.log(this.todoItem);
     }
 
-    onAddItem(){
+    onAddItem() {
 
-        let storedTodoList = JSON.parse(localStorage.getItem('todo-list'));
-        storedTodoList = storedTodoList === null ? [] : storedTodoList;
+        if (this.todoItem.length > 0) {
+            let storedTodoList = JSON.parse(localStorage.getItem('todo-list'));
+            storedTodoList = storedTodoList === null ? [] : storedTodoList;
 
-        storedTodoList.push(
-            {
-                id: new Date().valueOf(),
-                item: this.todoItem,
-                done: false
-            }
-        )
+            storedTodoList.push(
+                {
+                    id: new Date().valueOf(),
+                    item: this.todoItem,
+                    done: false
+                }
+            )
 
-        localStorage.setItem('todo-list', JSON.stringify(storedTodoList));
-        this.todoItem = '';
+            localStorage.setItem('todo-list', JSON.stringify(storedTodoList));
+            this.todoItem = '';
+        }
     }
 
-    render(props){
+    render() {
         return html`
         <div>
-            <input value=${this.todoItem} 
-                on-keyup="${(e) => this.inputKeypress(e)}" >
+            <input value = ${this.todoItem}
+                @keypress="${(e) => this.inputKeypress(e)}" />
             </input>
             <button @click="${this.onAddItem}"> Add Item </button>
         </div>
-        
         `;
+
     }
 }
 
